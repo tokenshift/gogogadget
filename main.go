378d229fa10368ebec31dd8e9da30693610f607d
Nathan Clark <Nathan.Clark@nasa.gov>
2016-02-23 09:04:55 -0500

    Hooking agent writing back up.

--- main.go
+++ main.go
@@ -11,44 +11,32 @@ import (
 )
 
 var (
-	inputFiles   = kingpin.Arg("sources", "Input source files.").Required().Strings()
-	packageName  = kingpin.Flag("package", "The output package name.").Short('p').Required().String()
-	interfaces   = kingpin.Flag("interface", "The name of the interface that will be wrapped.").Short('i').Required().Strings()
-	constructors = kingpin.Flag("constructor", "The name of a constructor that will be wrapped.").Short('c').Strings()
+	inputFile     = kingpin.Arg("source", "Input source files.").Required().String()
+	packageName   = kingpin.Flag("package", "The output package name.").Short('p').Required().String()
+	interfaceName = kingpin.Flag("interface", "The name of the interface that will be wrapped.").Short('i').Required().String()
+	constructors  = kingpin.Flag("constructor", "The name of a constructor that will be wrapped.").Short('c').Strings()
 )
 
 func main() {
 	kingpin.Parse()
 
-	fmt.Println(inputFiles)
-	fmt.Println(*packageName)
-	fmt.Println(interfaces)
-	fmt.Println(constructors)
+	parsed, err := parseFile(*inputFile)
+	fatalError(err)
 
-		/*
-		var parsed *ast.File
-		var writer AgentWriter
-		if inputFile != "" {
-			parsed, err = parseFile(inputFile)
-			fatalError(err)
-			writer = NewAgentWriter(interfaceName, packageName, parsed)
-		}
+	writer := NewAgentWriter(*interfaceName, *packageName, parsed)
 
-		WriteCodeGenerationWarning(os.Stdout)
-		writer.WritePackageName(os.Stdout)
+	WriteCodeGenerationWarning(os.Stdout)
+	writer.WritePackageName(os.Stdout)
 
-		if inputFile != "" {
-			writer.WriteAgentType(os.Stdout)
-		}
+	writer.WriteAgentType(os.Stdout)
 
-		for _, constructor := range constructors {
-			writer.WriteConstructor(os.Stdout, constructor)
-		}
+	for _, constructor := range *constructors {
+		writer.WriteConstructor(os.Stdout, constructor)
+	}
 
-		writer.WriteAgentMethods(os.Stdout)
+	writer.WriteAgentMethods(os.Stdout)
 
-		writer.WriteAgentControl(os.Stdout)
-		*/
+	writer.WriteAgentControl(os.Stdout)
 }
 
 func fatalError(err error) {
